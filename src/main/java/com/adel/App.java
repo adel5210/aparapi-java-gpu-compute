18bce6d5bdbd2a55a51e245dfa39c0834ddd798a
Adel <adel_5210m@yahoo.com>
2025-03-30 21:31:25 +0800

    Init commit

---
+++ src/main/java/com/adel/App.java
@@ -0,0 +1,39 @@
+package com.adel;
+
+import com.aparapi.Kernel;
+import com.aparapi.Range;
+
+import java.util.stream.IntStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        final int[] intA = IntStream.range(0, 1000).toArray();
+        final int[] intB = IntStream.range(100, 2000).toArray();
+        final int[] res = new int[intA.length];
+
+        {
+            long time = System.currentTimeMillis();
+            for (int i = 0; i < intA.length; i++) {
+                res[i] = intA[i] + intB[i];
+            }
+            System.out.println("Regular: " + (System.currentTimeMillis() - time));
+        }
+        {
+            long time = System.currentTimeMillis();
+            final Kernel kernel = new Kernel() {
+                @Override
+                public void run() {
+                    int i = getGlobalId();
+                    res[i] = intA[i] + intB[i];
+                }
+            };
+
+            final Range range = Range.create(res.length);
+            kernel.execute(range);
+            System.out.println("Aparapi: " + (System.currentTimeMillis() - time));
+        }
+    }
+}
